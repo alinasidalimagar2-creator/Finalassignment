5f036d229225739c77c14d4f2b3f6285f02b4b7b
alinasidalimagar2-creator <alinasidalimagar2@gmail.com>
2026-03-19 11:08:59 +0100

    Add redirect to home.php in index.php
    Redirect to home.php after establishing database connection.

--- Finalassignment/frontpage/index.php
+++ Finalassignment/frontpage/index.php
@@ -6,6 +6,9 @@ $user = "root";
 $pass = "";
 $db   = "student_course_hub";
 
+header("Location: home.php");
+exit;
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
